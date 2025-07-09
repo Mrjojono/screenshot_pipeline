486d0e3cff3ca46ac7d0150b6df447f5e2cf4edb
joan <nutsukpuijoan@gmail.com>
2025-07-09 11:16:51 +0200

    update yml

--- src/test/musique.test.js
+++ src/test/musique.test.js
@@ -10,7 +10,9 @@ expect.extend({ toMatchImageSnapshot });
 
 
 test('music UI should match the previous screenshot ', async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: true});
     const page = await browser.newPage();
 
 
